5d0a6c6390a28ebee18f1a8c33b0c53884885bd1
felixtanhm <felixtanhm@gmail.com>
2024-10-17 14:07:49 +0800

    stylinf

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
-function getJoke() {
-        try {
-            // Fetch the joke from the JokeAPI
-            const response = fetch('https://v2.jokeapi.dev/joke/?blacklistFlags=nsfw,religious,political,racist,sexist,explicit&type=single');
-            const data = response.json();
+async function getJoke() {
+    try {
+        // Fetch the joke from the JokeAPI
+        const response = await fetch('https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw,religious,political,racist,sexist,explicit&type=single');
+        const data = await response.json();
 
-            let jokeText = data.joke;
-            // Update the p tag with the joke
-            document.getElementById('joke').innerText = jokeText;
+        let jokeText = data.joke;
+        // Update the p tag with the joke
+        document.getElementById('joke').innerText = jokeText;
 
-        } catch (error) {
-            console.error('Error fetching joke:', error);
-            document.getElementById('joke').innerText = 'Failed to fetch a joke. Try again later!';
-        }
+    } catch (error) {
+        console.error('Error fetching joke:', error);
+        document.getElementById('joke').innerText = 'Failed to fetch a joke. Try again later!';
     }
+}
 
 // Add event listener to the button
 document.getElementById('getJokeBtn').addEventListener('click', () => {
-        getJoke();
-    });
+    getJoke();
+});
